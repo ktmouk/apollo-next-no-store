6ad36934a3dec02105663c6aad38523efa2f1f52
ktmouk <ktmouk@gmail.com>
2024-11-23 19:56:37 +0900

    chore: add fetch example

--- app/page.tsx
+++ app/page.tsx
@@ -5,3 +5,15 @@ export default async function Page() {
   const result = await getClient().query({ query: gql`query Query { allFilms { films { title } } }` });
   return <>{JSON.stringify(result.data)}</>
 }
+
+// The `fetch` function is working as expected.
+//
+// export default async function Page() {
+//   const result = await fetch("https://swapi-graphql.netlify.app/.netlify/functions/index", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json", },
+//     body: JSON.stringify({ query: `query Query { allFilms { films { title } } }` }),
+//     cache: "no-store",
+//   })
+//   return <>{JSON.stringify(await result.json())}</>
+// }
